3f26661e81c8e3f4439d9fbb81ff86c0ea37c591
David Grenier <davidgrenier@gmail.com>
2013-05-04 09:20:39 -0400

    Adds base solution

---
+++ Website/MyControl.fs
@@ -0,0 +1,10 @@
+﻿namespace Website
+
+open IntelliFactory.WebSharper
+open IntelliFactory.WebSharper.Html
+
+type MyControl() =
+    inherit Web.Control()
+
+    [<JavaScript>]
+    override this.Body = Div [] :> _
