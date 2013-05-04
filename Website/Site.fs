d8baa79aaf801edd211480efa8f00048a62b9d51
David Grenier <davidgrenier@gmail.com>
2013-05-04 09:26:53 -0400

    Properly wires up the site to be started

---
+++ Website/Site.fs
@@ -0,0 +1,23 @@
+﻿namespace Website
+
+open IntelliFactory.Html
+open IntelliFactory.WebSharper.Sitelets
+
+type Page =
+    | [<CompiledName "">] Home
+
+type Site () =
+    interface IWebsite<Page> with
+        member __.Actions = []
+        member __.Sitelet =
+            Sitelet.Infer <| function
+                | Home ->
+                    PageContent <| fun ctx ->
+                        {
+                            Page.Default with
+                                Title = Some "Striped"
+                                Body = [ Div []]
+                        }
+
+[<assembly: Website(typeof<Site>)>]
+do ()
